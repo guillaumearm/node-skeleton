900a69a3d07760ae749b56063176046f498c2c8a
Guillaume ARM <guillaume.arm@payfit.com>
2022-07-31 10:01:38 +0200

    feat: setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import express from 'express';
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on 'http://127.0.0.1:${PORT}'`);
+});
